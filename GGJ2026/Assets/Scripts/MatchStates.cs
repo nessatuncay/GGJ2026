28b62a853ec2b540876e0374e12d2ceebb31113c
lochleaf <200536258@student.georgianc.on.ca>
2026-01-31 10:49:43 -0500

    added enum to matchstate

--- GGJ2026/Assets/Scripts/MatchStates.cs
+++ GGJ2026/Assets/Scripts/MatchStates.cs
@@ -2,5 +2,13 @@ using UnityEngine;
 
 public class NewEmptyCSharpScript
 {
-    
+    class Match
+    {//class
+        public enum MatchState
+        {
+            Playing,
+            PlayerWon,
+            PlayerLost
+        }
+    }//end
 }
