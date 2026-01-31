3b9fc754f04df3ddda1efd699ee60e5f48ff4bf2
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-01-30 19:30:58 -0500

    created player script and added basic variables
    - auidenceMood
    - cardAmount
    - hasWon
    - hasLost

---
+++ GGJ2026/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        int auidenceMood = 2;
+        int cardAmount;
+        bool hasWon;
+        bool hasLost;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
