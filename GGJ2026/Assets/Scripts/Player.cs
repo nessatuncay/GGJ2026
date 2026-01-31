e6aeaf56d57d09c3aaee9cb0257b5d8b73c8cfdd
nessatuncay <144923042+nessatuncay@users.noreply.github.com>
2026-01-31 10:08:50 -0500

    created enum ActType
    - dancing
    - singing
    - acting

--- GGJ2026/Assets/Scripts/Player.cs
+++ GGJ2026/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        int auidenceMood = 2;
+        int audienceMood;
         int cardAmount;
         bool hasWon;
         bool hasLost;
@@ -14,6 +14,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Player player = GetComponent<Player>();
+        int audienceMood = 2;
+
+        if (audienceMood == 3)
+        {
+            bool hasWon = true;
+        }
+        else if(audienceMood == 0)
+        {
+            bool hasLost = true;
+        }
     }
 }
