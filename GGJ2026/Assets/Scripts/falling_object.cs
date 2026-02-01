933b4b1814a3c4af1e3d59c5eeeb24ecdb695787
lochleaf <200536258@student.georgianc.on.ca>
2026-02-01 03:07:22 -0500

    last commit

--- GGJ2026/Assets/Scripts/falling_object.cs
+++ GGJ2026/Assets/Scripts/falling_object.cs
@@ -24,7 +24,7 @@ public class falling_object : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (CompareTag("Player"))
         {
             Destroy(gameObject);
         }
--- GGJ2026/Assets/Scripts/test_player.cs
+++ GGJ2026/Assets/Scripts/test_player.cs
@@ -1,11 +1,54 @@
+using System;
 using UnityEngine;
 
 public class test_player : MonoBehaviour
 {
 
+    public float speed = 5f;
+    public float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // LEFT & RIGHT (Arrow keys)
+        if (Input.GetKey(KeyCode.A))
+            rb.linearVelocity = new Vector2(-speed, rb.linearVelocity.y);
+        else if (Input.GetKey(KeyCode.D))
+            rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
+        else
+            rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
+
+        
+        // JUMP (Space bar)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = false;
     
-
-
-
-
+    }
 }
+
+
+
+
+
+
