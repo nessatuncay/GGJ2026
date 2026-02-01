526ef8a603ca35c84c8d8932b67475c044ef9739
lochleaf <200536258@student.georgianc.on.ca>
2026-02-01 02:16:59 -0500

    working

---
+++ GGJ2026/Assets/Scripts/falling_object.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class falling_object : MonoBehaviour
+{
+    public float speed = 3f;
+    private float bottomLimit;
+
+    void Start()
+    {
+        // bottom of screen in world space
+        bottomLimit = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0)).y - 1f;
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        // If object goes off screen, destroy it
+       /* if (transform.position.y < bottomLimit)
+        {
+            Destroy(gameObject);
+        }*/
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
